35e48877b0c34de760c932b6e1c311be2afe530f
Ramon Ramos III <rj.ramos@gmail.com>
2016-07-12 01:02:24 -0400

    Create Go Button functionality

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 var recognition = new webkitSpeechRecognition();
 var goBtn = document.querySelector('button');
+var inputWord = document.querySelector('.input-word');
 
 function goButton() {
+  goBtn.disabled = true;
+  goBtn.textContent = "Listening";
   recognition.start();
 
   recognition.onresult = function(event) {
-    console.log("yes");
+    var word = event.results[0][0].transcript;
+    inputWord.textContent = word;
   }
+
+  recognition.onspeechend = function() {
+    recognition.stop();
+    goBtn.disabled = false;
+    goBtn.textContent = "Go";
+  }
+
+  recognition.onerror = function(event) {
+    goBtn.disabled = false;
+    goBtn.textContent = "Go";
+    goBtn.textContent = 'Start new test';
+    diagnosticPara.textContent = 'Error occurred in recognition: ' + event.error;
+  }
+
 }
 
 goBtn.addEventListener('click', goButton);
