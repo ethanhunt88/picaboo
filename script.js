7680d7b3d96e1440a0610cb912d0570402060a07
Ramon Ramos III <rj.ramos@gmail.com>
2016-07-12 10:39:14 -0400

    Display confidence level for testing

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 var recognition = new webkitSpeechRecognition();
 var goBtn = document.querySelector('button');
 var input = document.querySelector('.input');
+var confidence = document.querySelector('.confidence-level');
 var errorInfo = document.querySelector('.error-info');
+
+var flickrKey = "364e95c8cf3225977dfa93be089744d7";
+var flickrSecret = "ae571789c2e9c227";
 
 function goButton() {
   goBtn.disabled = true;
@@ -10,7 +14,10 @@ function goButton() {
 
   recognition.onresult = function(event) {
     var word = event.results[0][0].transcript;
+    var confidenceLevel = event.results[0][0].confidence;
     input.textContent = word;
+    confidence.textContent = confidenceLevel;
+    console.log(event);
   }
 
   recognition.onspeechend = function() {
